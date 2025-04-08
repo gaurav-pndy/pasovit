6598c38a6471ce37fed9314cf55c74efe0f339af
gaurav-pndy <gauravpndy00@gmail.com>
2025-04-08 12:38:51 +0530

    fixed issues

--- src/pages/Healthcare.jsx
+++ src/pages/Healthcare.jsx
@@ -97,7 +97,7 @@ const services = [
   {
     title: "Venous Port System Placement",
     description: "Installation of a venous port system for chemotherapy",
-    image: "src/assets/HealthcareImages/Venous Port System Placement.png",
+    image: "/Venous Port System Placement.png",
     link: "#",
   },
 ];
